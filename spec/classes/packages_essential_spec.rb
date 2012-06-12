abb3155af7956effcc3f01e839139439effd045f
Will Hopper <whopper@cat.pdx.edu>
2012-06-12 10:20:27 -0700

    Add spec tests for each rpmbuilder class

---
+++ spec/classes/packages_essential_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+describe 'rpmbuilder::packages::essential', :type => 'class' do
+
+  context "Packages which should be installed on every OS" do
+
+    packages = [
+    'createrepo',
+    'fedora-packager',
+    'make',
+    'mock',
+    'rpmdevtools',
+    'rpm-libs',
+    'rubygem-gem2rpm',
+    'rubygem-rake',
+    'yum',
+    'yum-utils',]
+
+    packages.each do|pkg|
+      it { should contain_package("#{pkg}").with_ensure('latest') }
+    end
+
+  end
+
+  context "On a Fedora OS" do
+    let(:facts) {{ :operatingsystem => 'fedora' }}
+
+    packages = ['rpm-sign']
+    packages.each do|pkg|
+       it { should contain_package("#{pkg}").with_ensure('latest') }
+    end
+  end
+end
---
+++ spec/classes/rpmbuilder_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe 'rpmbuilder', :type => 'class' do
+
+  context "The minimum components that should be included" do
+
+    it { should include_class("rpmbuilder::repos") }
+    it { should include_class("rpmbuilder::packages::essential") }
+
+  end
+
+  context "Extra components that may be included" do
+
+    let(:pre_condition) { 'class rpmbuilder::packages::extra {}' }
+    let(:pre_condition) { 'class mock {}' }
+    it { should include_class("rpmbuilder::packages::extra") }
+    it { should include_class("mock") }
+
+  end
+end
