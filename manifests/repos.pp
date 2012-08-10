25b624189cd25ef26cae0266e2692e86321a085e
Will Hopper <whopper@cat.pdx.edu>
2012-08-10 14:44:19 -0700

    Correct puppetlabs-products-source baseurl

--- manifests/repos.pp
+++ manifests/repos.pp
@@ -1,8 +1,18 @@
 class rpmbuilder::repos {
 
-  $baseurlOS = $operatingsystem ? {
-    'fedora'  => 'fedora',
-    default   => 'el',
+  case $operatingsystem {
+    'fedora': {
+      $baseurlOS      = 'fedora'
+      $os_version = $os_maj_version ? {
+        '15' => 'f15',
+        '16' => 'f16',
+        '17' => 'f17',
+      }
+    }
+    default: {
+      $baseurlOS = 'el'
+      $os_version = $os_maj_version
+    }
   }
 
   if $::osfamily == 'RedHat' and $::operatingsystem != 'Fedora' {
@@ -10,35 +20,35 @@ class rpmbuilder::repos {
    }
 
   yumrepo { 'puppetlabs-products':
-      baseurl   => "http://yum.puppetlabs.com/${baseurlOS}/${::os_maj_version}/products/${::architecture}",
+      baseurl   => "http://yum.puppetlabs.com/${baseurlOS}/${os_version}/products/${::architecture}",
       enabled   => '1',
       gpgcheck  => '1',
       gpgkey    => 'http://yum.puppetlabs.com/RPM-GPG-KEY-puppetlabs',
-      descr     => "Puppet Labs Products ${::os_maj_version} - ${::architecture}",
+      descr     => "Puppet Labs Products ${os_version} - ${::architecture}",
   }
 
   yumrepo { 'puppetlabs-deps':
-      baseurl   => "http://yum.puppetlabs.com/${baseurlOS}/${::os_maj_version}/dependencies/${::architecture}",
+      baseurl   => "http://yum.puppetlabs.com/${baseurlOS}/${os_version}/dependencies/${::architecture}",
       enabled   => '1',
       gpgcheck  => '1',
       gpgkey    => 'http://yum.puppetlabs.com/RPM-GPG-KEY-puppetlabs',
-      descr     => "Puppet Labs Dependencies ${::os_maj_version} - ${::architecture}",
+      descr     => "Puppet Labs Dependencies ${os_version} - ${::architecture}",
   }
 
   yumrepo { 'puppetlabs-products-source':
-      baseurl         => "http://yum.puppetlabs.com/${baseurlOS}/${::os_maj_version}/SRPMS",
+      baseurl         => "http://yum.puppetlabs.com/${baseurlOS}/${os_version}/products/SRPMS",
       failovermethod  => 'priority',
       enabled         => '1',
       gpgcheck        => '1',
       gpgkey          => 'http://yum.puppetlabs.com/RPM-GPG-KEY-puppetlabs',
-      descr           => "Puppet Labs Products ${::os_maj_version} - ${::architecture} - Source",
+      descr           => "Puppet Labs Products ${os_version} - ${::architecture} - Source",
   }
 
   yumrepo { 'puppetlabs-deps-source':
-      baseurl   => "http://yum.puppetlabs.com/${baseurlOS}/${::os_maj_version}/dependencies/SRPMS",
+      baseurl   => "http://yum.puppetlabs.com/${baseurlOS}/${os_version}/dependencies/SRPMS",
       enabled   => '1',
       gpgcheck  => '1',
       gpgkey    => 'http://yum.puppetlabs.com/RPM-GPG-KEY-puppetlabs',
-      descr     => "Puppet Labs Source Dependencies ${::os_maj_version} - Source",
+      descr     => "Puppet Labs Source Dependencies ${os_version} - Source",
   }
 }
